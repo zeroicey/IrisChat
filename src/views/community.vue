6eacf26222f8893b6861c5ff8066e1ece57dc5e9
Jerry-Guo1020 <2593517388@qq.com>
2025-07-18 18:42:06 +0800

    feat(jerry-web):try to configure the router and then set up two page views

---
+++ src/views/community.vue
@@ -0,0 +1,11 @@
+<template>
+<h1>this is community page</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,23 @@
+<template>
+<h1>hello,this is home page</h1>
+<div>fasjfkajfkasjfkasfj</div>
+<button @click="tocommunity">跳</button>
+</template>
+
+<script setup>
+import {ref} from "vue"
+import { RouterView } from "vue-router";
+import router from "../router";
+
+
+const tocommunity= ()=> {
+    router.push('/community')
+}
+</script>
+
+<style>
+* {
+    padding: 0;
+    margin: 0;
+}
+</style>
